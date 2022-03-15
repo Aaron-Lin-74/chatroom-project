8a498f760ec9662c51210732bfe033e483547b19
Aaron Lin <aaronlin74@gmail.com>
2022-03-15 17:06:34 +1100

    Added test suite of ChatRoom component and updated the code

---
+++ src/components/ChatRoom.test.tsx
@@ -0,0 +1,34 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ChatRoom from './ChatRoom';
+
+describe('ChatRoom component', () => {
+  test('should match the snapshot', () => {
+    const { asFragment } = render(<ChatRoom />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('should render the component', () => {
+    render(<ChatRoom />);
+    screen.getByText(/show more/i);
+  });
+
+  test('should hide the span of show more when click it', () => {
+    render(<ChatRoom />);
+    fireEvent.scroll(screen.getByRole('main'), { target: { scrollY: 0 } });
+    const spanElement = screen.getByText(/show more/i);
+    fireEvent.click(spanElement);
+    expect(spanElement).toHaveStyle('display: none');
+  });
+
+  test('should show the span of show more when scroll top', () => {
+    render(<ChatRoom />);
+    const spanElement = screen.getByText(/show more/i);
+    fireEvent.click(spanElement);
+    expect(spanElement).toHaveStyle('display: none');
+    fireEvent.scroll(screen.getByRole('main'), {
+      target: { scrollY: 0 },
+    });
+    expect(spanElement).toHaveStyle('display: block');
+  });
+});
